f505a1f8c9042bad653e6b4e0e1539acf29cf6b8
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2024-11-07 21:53:57 +0100

    Rework login

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 import puppeteer from 'puppeteer'
 
 /**
- * @param {puppeteer.Page} page
  * @param {string} login
  * @param {string} password
  * 
  * @returns {Promise}
  */
-export default function (page, login, password) {
+export default function (login, password) {
 
     return new Promise(async (resolve, reject) => {
         
         try {
-            await page.goto('https://dashboard.heropost.io/login')
+            const browser = await puppeteer.launch({
+                headless: false,
+                args: [
+                    '--disable-notifications',
+                    '--no-sandbox',
+                    '--window-size=1600,900'
+                ]
+            })
 
-            const loginInputSelector = '[name="email"]'
-            const passwordInputSelector = '[name="password"]'
+            const pages = await browser.pages()
+
+            /** @var {puppeteer.Page} page */
+            let page
+
+            if (! pages) {
+                page = await browser.newPage()
+            } else {
+                page = pages[0]
+            }
+
+            await page.setViewport({
+                width: 1600,
+                height: 900,
+                eviceScaleFactor: 1
+            })
+
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 OPR/114.0.0.0')
+            await page.goto('https://login.heropost.io/Account/Login')
+
+            const loginInputSelector = '#Email'
+            const passwordInputSelector = '#Password'
             await page.waitForSelector(loginInputSelector)
             await page.waitForSelector(passwordInputSelector)
 
@@ -24,13 +50,39 @@ export default function (page, login, password) {
                 document.querySelector(passwordInputSelector).value = password
             }, login, password, loginInputSelector, passwordInputSelector)
             
-            const submitButtonSelector = 'button[type="submit"]'
+            const submitButtonSelector = 'button[name="button"][value="login"]'
 
             await page.evaluate(submitButtonSelector => {
                 document.querySelector(submitButtonSelector).click()
             }, submitButtonSelector)
 
-            await page.waitForSelector('.user-name')
+            try {
+                await page.waitForSelector('.MuiTypography-h1')
+            } catch (e) {
+                const errorSelector = '.validation-summary-errors'
+                const errorText = await page.evaluate(errorSelector => {
+                    const errorBox = document.querySelector(errorSelector)
+
+                    if (! errorBox) {
+                        return null
+                    }
+
+                    const errorText = errorBox.innerText
+
+                    if (! errorText) {
+                        return null
+                    }
+
+                    return errorText
+                }, errorSelector)
+
+                await browser.close()
+                throw Error(errorText)
+            }
+
+            await page.waitForTimeout(90000)
+            await page.waitForTimeout(90000)
+            await page.waitForTimeout(90000)
 
             resolve()
         } catch (e) {
--- test.example.js
+++ test.example.js
@@ -1,20 +1,10 @@
 // Copy this file to test.js
 // You can then test using npm test
 
-import puppeteer from 'puppeteer'
 import login from './index.js'
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-            '--disable-notifications',
-            '--no-sandbox'
-        ]
-    })
-    const page = await browser.newPage()
     console.log(await login(
-        page,
         'Heropost login or email',
         'Heropost password',
     ))
